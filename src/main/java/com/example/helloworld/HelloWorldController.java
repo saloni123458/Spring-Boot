5b20e9cb0e441067a80d5709ebc1281157108a26
saloni123458 <garjesaloni@gmail.com>
2024-03-12 13:59:05 +0000

    Video Code - Hello Spring Boot

---
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to Spring Boot World and Hello World";
+    }
+}
